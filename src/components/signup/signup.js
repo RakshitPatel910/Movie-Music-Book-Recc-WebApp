2195a76a82606f45d19bb6bf047c65cd6449834a
Satyam <satyam.ms@somaiya.edu>
2022-08-13 12:25:46 +0530

    new chjanges

--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import customCss from './style.js'
+// import { makeStyles } from "@material-ui/core/styles";
 // import { useState } from "react"
-function Signup(){
-    // const {isLogin,setIsLogin} = useState(false);
-    return(
-        <>
-                <div className="signup-container">
-                    <input type="email" name="" id="" placeholder="Enter Your Email"/>
-                    <input type="password" name="" id="" placeholder="Enter password"/>
-                    <button type="submit" >submit</button>
-                </div>
-        </>
-    )
+function Signup() {
+  // const {isLogin,setIsLogin} = useState(false);
+  const classes = customCss()
+  return (
+    <>
+      <div className="signup-container">
+        <TextField id="outlined-basic" label="Email" variant="outlined" /> 
+        <TextField
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+        />
+        <Button className={classes.Button} variant="contained">Login</Button>
+      </div>
+    </>
+  );
 }
 
-export default Signup
+export default Signup;
--- src/components/signup/style.js
+++ src/components/signup/style.js
@@ -1,1 +1,11 @@
-export default signStyle()
+// import * as React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+// import Button from "@mui/material/Button";
+import { red } from '@mui/material/colors';
+
+export default makeStyles(()=>({
+    Button:{
+        backgroundcolor: red,
+    }
+}));
+
