516b17a8637f133e0fdeb02dc3fffa28c6e8a4ea
Satyam <satyam.ms@somaiya.edu>
2022-08-16 12:05:32 +0530

    password functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import {useEffect} from 'react';
+import jwt_decode from 'jwt-decode'
 import './App.css';
-import Signup from './components/signup/signup.js';
+// import Signup from './components/signup/signup.js';
 // import Signin from './components/signin/signin.js'
 function App() {
+  const google = window.google
+  function handleCallbackResponse(res){
+    console.log("encoded JWT ID token"+ res.credential);
+    var userObject = jwt_decode(res.credential); 
+    console.log(userObject)
+  }
+  
+  useEffect(()=>{
+    // /* global google*/
+    google.accounts.id.initialize({
+      client_id:
+        "472882567063-3budp9p8d63nn03uv9vsn3ihkpuau6ur.apps.googleusercontent.com",
+        callback: handleCallbackResponse
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      {theme:"outline",size:"large"}
+    )
+
+  },[])
+
   return (
     <>
-      <Signup/>
+      {/* <Signup/> */}
+      <div id="signInDiv"></div>
     </>
       
   );
--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -1,9 +1,22 @@
 import React from "react";
 import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
 import TextField from "@mui/material/TextField";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import { useState } from "react";
 import './style.css'
 
 function Signup(){
+
+  const [passwordShown,setPasswordShown] = useState(false);
+
+  const togglePassword=()=>{
+    setPasswordShown(!passwordShown)
+    console.log(passwordShown)
+  };
+  
+
     return (
       <>
         <div className="signup">
@@ -11,21 +24,22 @@ function Signup(){
             <TextField className="firstName" label="First Name" />
             <TextField className="middleName"  label="Middle Name" style={{margin:"auto 1.5vh"}} />
             <TextField className="lastName" label="Last Name" />
-          </div>
+          </div> 
           <TextField className="email" label="Email" style={{marginTop:"0vh",marginBottom:"2vh"}}/>
           <TextField
             className="password"
             label="Password"
-            type="password"
+            type={passwordShown ? "text" : "password"}
             autoComplete="current-password"
             style={{marginBottom:"2vh"}}
           />
+          <IconButton onClick={togglePassword} style={{color:"black"}} >{passwordShown ? <VisibilityOffIcon/> : <VisibilityIcon/> }</IconButton>
           <TextField className="password" label="Confirm Password" style={{marginBottom:"2vh"}}/>
-          <Button className="button" style={{backgroundColor:"black",marginBottom:"2vh"}} variant="contained" >Sign Up</Button>
+          <Button className="button" style={{backgroundColor:"black",marginBottom:"2vh"}} variant="contained" >Sign Up</Button> 
           <Button className="googleButton" style={{backgroundColor:"black"}} variant="contained">Signin with google</Button>
         </div>
-      </>
+      </> 
     );
-}
-
+}  
+ 
 export default Signup
