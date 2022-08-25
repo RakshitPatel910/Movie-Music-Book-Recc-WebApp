9e28f7fd5b91ad734d052743dba65581df887953
Satyam <satyam.ms@somaiya.edu>
2022-08-25 22:42:39 +0530

    home page start

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,17 +1,23 @@
-import React from "react";
+import * as React from "react";
 import axios from "axios";
 import { useState,useEffect} from "react";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+
+import './style.css'
+
+
 const key = "f20575175c2deae7974eb547727d1ace";
 const id = 550
 // const path = `https://image.tmdb.org/t/p/w185${list.poster_path}`
-export default function Home(){
+function Home(){
 
     
     const [image,setImage] = useState("");
-    const [overview,setOverview] = useState("");
-    const [genre,setGenre] = useState([]);
-    const [title,setTitle] = useState("");
-    const [review,setReview] = useState([])
+    // const [overview,setOverview] = useState("");
+    // const [genre,setGenre] = useState([]);
+    // const [title,setTitle] = useState("");
+    // const [review,setReview] = useState([])
   
     
 
@@ -21,22 +27,12 @@ export default function Home(){
         `https://api.themoviedb.org/3/movie/${id}?api_key=${key}&language=en-US`
       );
 
-    const fetchReview = () =>
-      axios.get(
-        `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${key}&language=en-US&page=1`
-      );
+    // const fetchReview = () =>
+    //   axios.get(
+    //     `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${key}&language=en-US&page=1`
+    //   );
 
-      // const getImage = async ()=>{
-      //   const image1 = await fetchImage();
-      //   console.log(image1.data.backdrop_path);
-      //   setImage(image1.data.backdrop_path);
-      //   console.log(image+"image")
-      // } 
 
-      // useEffect(()=>{
-      //   const image2  = getImage();
-      //   console.log(image2)
-      // },[])
 
 
       useEffect(()=>{
@@ -47,21 +43,21 @@ export default function Home(){
           if(isMounted){
             const path = result.data.poster_path;
             setImage(`https://image.tmdb.org/t/p/w185${path}`);
-            setOverview(result.data.overview);
-            setGenre(result.data.genres);
-            setTitle(result.data.title)
+            // setOverview(result.data.overview);
+            // setGenre(result.data.genres);
+            // setTitle(result.data.title)
           }
         }
 
-        async function getReview(){
-          const result = await fetchReview()
-          if(isMounted){
-            setReview(result.data.results);
-          }
-        }
+        // async function getReview(){
+        //   const result = await fetchReview()
+        //   if(isMounted){
+        //     setReview(result.data.results);
+        //   }
+        // }
 
         getMovie();
-        getReview()
+        // getReview()
         return()=>{
           isMounted = false;
         };
@@ -69,41 +65,39 @@ export default function Home(){
       
 
 
-
-     
-
     return (
       <>
-        <div>
-          <img src={image} alt="" />
-        </div>
-        <div>
-          <h1>Title</h1>
-          {title}
-        </div>
-        <div>
-          <h1>Overview</h1>
-          {overview}
-        </div>
-        <div>
-          <h1>genre</h1>
-          {genre.map((g) => (
-            <div>{g.name}</div>
-          ))}
-        </div>
-        <div>
-          <h1>Review</h1>
-          {
-            review.map(r=>(
-              <>
-                
-                <h3>{r.author}</h3>
-                <div>{r.content}</div>
-              </>
-            ))
-          }
-        </div>
+        <Container
+          maxWidth=""
+          sx={{
+            height: "fit-content",
+            bgcolor: "red",
+            display: "flex",
+            flexDirection: "row",
+            padding: "10px 50px",
+          }}
+        >
+          <Box
+            className="movieInfo"
+            sx={{ margin: "10px 0px", height: "65vh", width: "40%" }}
+          >
+            <img src={image} alt="" className="poster" />
+          </Box>
+          <Box
+            className="movieInfo"
+            sx={{
+              margin: "10px 0px",
+              height: "65vh",
+              width: "60%",
+              boxShadow: "0 0 80px 80px black",
+            }}
+          >
+            <img src={image} alt="" className="poster" style={{}} />
+          </Box>
+        </Container>
       </>
     );
 
 }
+
+export default Home;
