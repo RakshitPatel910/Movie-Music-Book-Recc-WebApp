797ac46cf59fd76ab6ace8748ad72104981c511b
Satyam <satyam.ms@somaiya.edu>
2022-08-27 18:29:57 +0530

    movieInfo changes

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,22 +1,27 @@
 import * as React from "react";
 import axios from "axios";
 import { useState,useEffect} from "react";
-import Box from "@mui/material/Box";
+// import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
+import AddIcon from "@mui/icons-material/Add";
 
 import './style.css'
 
 
 const key = "f20575175c2deae7974eb547727d1ace";
 const id = 550
+// const id = 278;
+// const id = 244786;
 // const path = `https://image.tmdb.org/t/p/w185${list.poster_path}`
 function Home(){
 
     
     const [image,setImage] = useState("");
-    // const [overview,setOverview] = useState("");
-    // const [genre,setGenre] = useState([]);
-    // const [title,setTitle] = useState("");
+    const [overview,setOverview] = useState("");
+    const [genre,setGenre] = useState([]);
+    const [title,setTitle] = useState("");
+    const [runTime,SetRunTime] = useState("")
+    const [releaseDate,setReleaseDate] = useState("")
     // const [review,setReview] = useState([])
   
     
@@ -43,9 +48,11 @@ function Home(){
           if(isMounted){
             const path = result.data.poster_path;
             setImage(`https://image.tmdb.org/t/p/w185${path}`);
-            // setOverview(result.data.overview);
-            // setGenre(result.data.genres);
-            // setTitle(result.data.title)
+            setOverview(result.data.overview);
+            setGenre(result.data.genres);
+            setTitle(result.data.title)
+            SetRunTime(result.data.runtime);
+            setReleaseDate(result.data.release_date);
           }
         }
 
@@ -77,30 +84,44 @@ function Home(){
             padding: "10px 50px",
           }}
         >
-          <Box
-            className="movieInfo"
-            sx={{
-              margin: "10px 0px",
-              height: "65vh",
-              width: "40%",
-            }}
-          >
-            <img
-              src={image}
-              alt=""
-              className="poster"
-            />
-          </Box>
-          <Box
-            className="movieInfo"
-            sx={{
-              margin: "10px 0px",
-              height: "65vh",
-              width: "60%",
-            }}
-          >
-            {/* <img src={image} alt="" className="poster" style={{}} /> */}
-          </Box>
+          <div class="movie_card" id="bright">
+            <div class="info_section">
+              <div class="movie_header">
+                <img class="locandina" alt="movieImage" src={image} />
+                <h1>{title}</h1>
+                <h4>{releaseDate.slice(0,4)}, David Ayer</h4>
+                <span class="minutes">{runTime} min</span>
+                <p class="type">
+                  {genre.map((e) => (
+                    <>{`${e.name} `}</>
+                  ))}
+                </p>
+              </div>
+              <div class="movie_desc">
+                <p class="text">{overview}</p>
+              </div>
+              <div class="movie_social">
+                <ul>
+                  <li>
+                    <i class="material-icons"><AddIcon sx={{height:"60px"}}/></i>
+                  </li>
+                  <li>
+                    <i class="material-icons"></i>
+                  </li>
+                  <li>
+                    <i class="material-icons">chat_bubble</i>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div
+              class="blur_back bright_back"
+              style={{
+                backgroundImage: `url(${image})`,
+                backgroundSize: "cover",
+              }}
+            ></div>
+          </div>
         </Container>
       </>
     );
