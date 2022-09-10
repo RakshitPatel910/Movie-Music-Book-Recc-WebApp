e9108438ca7b03c67b74610a59846b5af1b86dd2
Satyam <satyam.ms@somaiya.edu>
2022-09-10 22:43:22 +0530

    description page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 // import Signup from './components/signup/signup.js';
 // import Signin from './components/signin/signin.js'
-import Carousel from './components/Carousel/Carousel';
+// import Carousel from './components/Carousel/Carousel';
  import Home from './components/home/home.js' 
 function App() {
   return (
