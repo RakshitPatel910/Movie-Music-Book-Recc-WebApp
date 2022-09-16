f452efe3a18123332b0d4d57e5a6163e77f64f1d
RakshitPatel910 <86058505+RakshitPatel910@users.noreply.github.com>
2022-09-16 19:00:19 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Signup from './components/signup/signup.js';
-import Signin from './components/signin/signin.js';
+import Signup from './components/Signup/Signup.js';
+import Signin from './components/Signin/Signin.js';
 import Carousel from './components/Carousel/Carousel';
 import Home from './components/Home/Home.js';
 import SearchResults from './components/SearchResults/SearchResults'; 
