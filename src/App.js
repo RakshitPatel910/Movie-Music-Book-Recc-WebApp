36153056061f21e14d6fcffb2b1bb1b0185e0396
RakshitPatel910 <86058505+RakshitPatel910@users.noreply.github.com>
2022-09-16 19:07:09 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,16 +2,11 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-<<<<<<< HEAD
-import Signup from './components/Signup/Signup.js';
-import Signin from './components/Signin/Signin.js';
-=======
 import Navbar from './components/Appbar/Navbar/Navbar.js';
 import Searchbar from './components/Appbar/Searchbar/Searchbar.js';
 import { CheckBoxOutlineBlank } from '@material-ui/icons';
-import Signup from './components/signup/signup.js';
-import Signin from './components/signin/signin.js';
->>>>>>> e16340ae482c0a96502f7b3847482397c1e0e419
+import Signup from './components/Signup/Signup.js';
+import Signin from './components/Signin/Signin.js';
 import Carousel from './components/Carousel/Carousel';
 import Home from './components/Home/Home.js';
 import SearchResults from './components/SearchResults/SearchResults'; 
