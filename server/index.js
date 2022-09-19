bbe48a472766268fff285dc21029769e5afee535
Satyam <satyam.ms@somaiya.edu>
2022-09-19 22:26:19 +0530

    userschema created

--- server/index.js
+++ server/index.js
@@ -5,8 +5,9 @@ const express = require('express');
 const app = express()
 
 dotenv.config({path:'./config.env'})
-require('./db/conn')
 
+require('./db/conn')
+const User = require('./model/userSchema')
 
 const port = process.env.PORT;
 // %40 = @  username = zakespy password= Exodus@123
