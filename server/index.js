7ec2c07016c11525093716bcdef2bdaa20a4c4aa
Satyam <satyam.ms@somaiya.edu>
2022-09-19 22:11:53 +0530

    .env file created

--- server/index.js
+++ server/index.js
@@ -1,28 +1,23 @@
+const dotenv = require('dotenv')
 const { application } = require('express');
-const mongoose = require('mongoose')
+
 const express = require('express');
 const app = express()
 
-const DB =
-  'mongodb+srv://zakespy:Exodus%40123@cluster0.yc04396.mongodb.net/moviecon?retryWrites=true&w=majority';
-// %40 = @  
+dotenv.config({path:'./config.env'})
+require('./db/conn')
 
-mongoose.connect(DB 
-    // , {
-    // useNewUrlParser: true,
-    // useCreateIndex:true,
-    // useUnifiedTopology:true,
-    // useFindAndModify:false
-// }
-).then(()=>{
-    console.log("Connection is successsful")
-}).catch((err)=> console.log(err))
 
-app.get('/',(req,res)=>{
+const port = process.env.PORT;
+// %40 = @  username = zakespy password= Exodus@123
+
+
+
+app.get('/',(req,res)=>{ 
     res.send('Hello world hui')
 })
-
-app.listen(3010,()=>{ 
+ 
+app.listen(port,()=>{ 
     console.log('serve is running at port 3010')
 })
 
