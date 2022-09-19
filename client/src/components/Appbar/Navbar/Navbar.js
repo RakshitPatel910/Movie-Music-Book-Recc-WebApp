c1af20b5ef50dd908d84c180cd35eb2fca440626
RakshitPatel910 <86058505+RakshitPatel910@users.noreply.github.com>
2022-09-19 13:38:49 +0530

    server folder added

---
+++ client/src/components/Appbar/Navbar/Navbar.js
@@ -0,0 +1,169 @@
+import * as React from "react";
+import { Avatar, Button } from "@mui/material";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import CssBaseline from "@mui/material/CssBaseline";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+//import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import ExitToAppOutlinedIcon from "@mui/icons-material/ExitToAppOutlined";
+import AssessmentRoundedIcon from "@mui/icons-material/AssessmentRounded";
+import ContactlessIcon from "@mui/icons-material/Contactless";
+//import AddAPhotoTwoToneIcon from '@mui/icons-material/AddAPhotoTwoTone';
+import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
+import FlashOnOutlinedIcon from '@mui/icons-material/FlashOnOutlined';
+import { Link } from 'react-router-dom';
+import AbcRoundedIcon from '@mui/icons-material/AbcRounded';
+
+
+
+const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginRight: drawerWidth,
+  }),
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-start",
+}));
+
+export default function PersistentDrawerRight() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  
+
+  return (
+    
+    
+   
+    <Box sx={{ display: 'flex' }} marginRight={3} marginLeft={3} marginBottom={3} marginTop={1}  >
+      <CssBaseline />
+      <AppBar style={{background: "#5579C6" , borderRadius: '20px'}}   elevation={0} open={open}  >
+        <Toolbar >
+        <IconButton size='large' color='inherit'>
+                    <ContactlessIcon />
+
+                    </IconButton>
+                    <Typography component={Link} to='/' variant='h4'  sx={{flexGrow:1,textDecoration:"none",color:"white"}}>
+                        Movicon
+                    </Typography>
+                    
+        
+          <Avatar
+            color="inherit"
+            aria-label="open drawer"
+            edge="end"
+            onClick={handleDrawerOpen}
+            sx={{ ...(open && { display: "none" }) }}
+          >
+            <Avatar>
+              <FlashOnOutlinedIcon />
+            </Avatar>
+
+
+                    
+                    <Avatar></Avatar>
+                    
+                    
+
+
+          </Avatar>
+          
+          
+        </Toolbar>
+      </AppBar>
+
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+          },
+        }}
+        variant="persistent"
+        anchor="right"
+        open={open}
+      >
+        <DrawerHeader  sx={{background: "#7697A0"}}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "rtl" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+
+        <Button>
+
+
+        <Avatar ></Avatar>
+        </Button>
+
+        <Button color="inherit">
+          <Typography variant="h5">USERNAME</Typography>
+        </Button>
+        <Divider />
+
+        <List>
+          {['MY ACCOUNT', 'SIGN OUT', 'APP INFO'].map((text, index) => (
+            <ListItem style={{ color: "black" }} key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? (
+                    <AssessmentRoundedIcon />
+                  ) : (
+                    <ExitToAppOutlinedIcon />
+                  )}
+                </ListItemIcon>
+                <ListItemText primary={text}  />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
+    
+
+  );
+}
+// hello
---
+++ client/src/components/Appbar/Searchbar/Searchbar.js
@@ -0,0 +1,171 @@
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import AnchorIcon from '@mui/icons-material/Anchor';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import AccountTreeIcon from '@mui/icons-material/AccountTree';
+import ColorLensIcon from '@mui/icons-material/ColorLens';
+import { Button,Avatar,FormControl,OutlinedInput,value,handleChange,InputAdornment } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+
+const drawerWidth = 240;
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(0),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+
+
+
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
+export default function PersistentDrawerLeft() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+
+  return ( 
+    <Box sx={{ display: 'flex' }}  >
+      <CssBaseline />
+      
+      <Grid container spacing={1} columns={10}>
+      <Grid item xs={0.25}><Item></Item></Grid>
+      
+        <Grid item xs={0.42}>
+          <Item>
+          <IconButton         
+          color="inherit"
+          aria-label="open drawer"
+          onClick={handleDrawerOpen}
+          edge='end'
+          sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          
+        >
+          <MenuIcon />
+        </IconButton>
+          </Item>
+        </Grid>
+        <Grid item xs={1.6}><Item></Item></Grid>
+        
+        <Grid item xs={7.4}>
+          <Item >
+          <FormControl fullWidth id="fullWidth" variant="outlined">
+          <OutlinedInput
+          
+            placeholder="Search"
+            endAdornment={<InputAdornment position="end"><Avatar>< SearchIcon/></Avatar></InputAdornment>}
+              />
+        </FormControl> 
+            </Item>
+        </Grid>
+      </Grid>
+     
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+            
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader sx={{background: "#7697A0"}}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+          
+        </DrawerHeader>
+        
+        <Divider />
+        <List>
+          <ListItemButton>
+            <ListItem>
+          <ListItemIcon>
+          <AnchorIcon />
+          </ListItemIcon>
+            <ListItemText style={{ color: "black" }} primary='ACTION' />
+          </ListItem>          
+          </ListItemButton>
+
+          <ListItemButton>
+            <ListItem>
+          <ListItemIcon>
+          <AcUnitIcon />
+          </ListItemIcon>
+            <ListItemText style={{ color: "black" }} primary='THRILLER' />
+          </ListItem>          
+          </ListItemButton>
+
+          <ListItemButton>
+            <ListItem>
+          <ListItemIcon>
+          <FavoriteBorderIcon />
+          </ListItemIcon>
+            <ListItemText style={{ color: "black" }} primary='ROMANCE' />
+          </ListItem>          
+          </ListItemButton>
+
+          <ListItemButton>
+            <ListItem>
+          <ListItemIcon>
+          <AccountTreeIcon />
+          </ListItemIcon>
+            <ListItemText style={{ color: "black" }} primary='ADVENTURE' />
+          </ListItem>          
+          </ListItemButton>
+
+          <ListItemButton>
+            <ListItem>
+          <ListItemIcon>
+          <ColorLensIcon />
+          </ListItemIcon>
+            <ListItemText style={{ color: "black" }} primary='DRAMA' />
+          </ListItem>          
+          </ListItemButton>
+
+        </List>
+       
+      </Drawer>
+    </Box>
+  );
+}
